b79fb9449d80031357c97e8aae87dda81869f708
Igor Costa <icosta@allurion.com>
2022-07-14 11:12:14 -0300

    fix: add locale to the request

--- src/templates/index.js
+++ src/templates/index.js
@@ -1,12 +1,12 @@
-import React, { useState, useEffect } from "react"
-import { useParams } from "react-router-dom"
-import Layout from "../containers/Layout"
-import FeaturesSection from "../components/FeaturesSection"
-import HeroSection from "../components/HeroSection"
-import TestimonialsSection from "../components/TestimonialsSection"
-import BlogSection from "../components/BlogSection"
-import TwoColumnWithImageSection from "../components/TwoColumnWithImageSection"
-import NotFoundSection from "../components/NotFoundSection"
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import Layout from "../containers/Layout";
+import FeaturesSection from "../components/FeaturesSection";
+import HeroSection from "../components/HeroSection";
+import TestimonialsSection from "../components/TestimonialsSection";
+import BlogSection from "../components/BlogSection";
+import TwoColumnWithImageSection from "../components/TwoColumnWithImageSection";
+import NotFoundSection from "../components/NotFoundSection";
 import SEO from "../components/SEO";
 import Spinner from "../components/Spinner";
 import { butterCMS } from "../utils/buttercmssdk";
@@ -24,23 +24,29 @@ const IndexPage = () => {
   useEffect(() => {
     const loadData = async () => {
       try {
-        const page = await butterCMS.page.retrieve("landing-page", slug || "landing-page-with-components")
-        setPage(page.data.data)
+        const page = await butterCMS.page.retrieve(
+          "landing-page",
+          slug || "landing-page-with-components",
+          {
+            locale: "en",
+          }
+        );
+        setPage(page.data.data);
       } catch (error) {
-        setError(true)
+        setError(true);
       }
 
       // Returns a list of published posts. The posts are returned sorted by publish date, with the most recent posts appearing first.
-      const posts = await butterCMS.post.list({ page: 1, page_size: 2 })
-      setBlogPosts(posts.data.data)
+      const posts = await butterCMS.post.list({ page: 1, page_size: 2 });
+      setBlogPosts(posts.data.data);
       setLoader(false);
-    }
+    };
 
-    loadData()
+    loadData();
   }, []);
 
-  if (error) return (<NotFoundSection />)
-  if (loader) return (<Spinner />)
+  if (error) return <NotFoundSection />;
+  if (loader) return <Spinner />;
 
   return (
     <Layout menuItems={menuItems}>
@@ -49,21 +55,23 @@ const IndexPage = () => {
       {page.fields.body.map((bodyElement, i) => {
         switch (bodyElement.type) {
           case "hero":
-            return <HeroSection fields={bodyElement.fields} key={i} />
+            return <HeroSection fields={bodyElement.fields} key={i} />;
           case "two_column_with_image":
-            return <TwoColumnWithImageSection fields={bodyElement.fields} key={i} />
+            return (
+              <TwoColumnWithImageSection fields={bodyElement.fields} key={i} />
+            );
           case "features":
-            return <FeaturesSection fields={bodyElement.fields} key={i} />
+            return <FeaturesSection fields={bodyElement.fields} key={i} />;
           case "testimonials":
-            return <TestimonialsSection fields={bodyElement.fields} key={i} />
+            return <TestimonialsSection fields={bodyElement.fields} key={i} />;
           default:
-            return null
+            return null;
         }
       })}
 
       <BlogSection blogPosts={blogPosts} />
     </Layout>
-  )
-}
+  );
+};
 
-export default IndexPage
+export default IndexPage;
